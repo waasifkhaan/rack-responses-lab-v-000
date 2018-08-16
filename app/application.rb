9781f39ae3cab36bae1cb8a3d840d1de269ca40d
Wasif Khan <waasif.khaan@gmail.com>
2018-08-16 21:34:05 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    
+   if select_datetime
+  end 
+    if num_1 == num_2 && num_2 == num_3 
+      resp.write "You Win"
+    else 
+      resp.write "You Lose"
+    end 
+    resp.finish
+  end
+
+end
